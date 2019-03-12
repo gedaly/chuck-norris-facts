7b96eec91e2df92845d856d9cb0d825332b4eb8d
gedaly <1335956+gedaly@users.noreply.github.com>
2019-03-11 21:44:42 -0700

    Moved the API call to its own function

--- chuck-norris-facts.php
+++ chuck-norris-facts.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: Chuck Norris Facts
-Description: Chuck Norris once created the best WordPress plugin ever. This isn't it, but we honor him and his awesomeness. Uses the API from https://api.chucknorris.io/
+Description: Chuck Norris once created the best WordPress plugin ever. This isn't it, but we honor him and his awesomeness with a random fact that is 110% totally true.
 Version: 1.0.0
 Author: Gedaly
 Author URI: https://geda.ly
@@ -10,34 +10,26 @@ License: GPLv2 or later
 Text Domain: chuck-norris-facts
 */
 
-// Plugin Github Repo: https://github.com/gedaly/chuck
-
 /*
+Plugin Github Repo: https://github.com/gedaly/chuck-norris-facts
 
 Chuck Norris once wrote a terms & conditions statement. No lawyers survived.
-
 */
 
+add_action( 'admin_notices', 'gg_chuck_admin_notice' );
+
 function gg_chuck_admin_notice() {
-    $get_quote = file_get_contents('https://api.chucknorris.io/jokes/random');
-
-    $get_result  = json_decode($get_quote);
-
-    $quote = $get_result->value;
-
     ?>
     <div class="notice notice-info is-dismissible" style="display:flex;align-items:center;">
         <img src="<?php echo plugins_url( '/img/chuck-image.jpg', __FILE__ ); ?>" height="32px" width="32px" style="display:inline-block;padding-right:10px;">
-        <p><?php echo $quote ?></p>
+        <p><?php gg_chuck_get_quote() ?></p>
     </div>
     <?php
 }
-add_action( 'admin_notices', 'gg_chuck_admin_notice' );
 
-
-function gg_chuck_getQuote($url) {
-    $response = wp_remote_get( $url );
-    $body = wp_remote_retrieve_body( $response );
-    $body = substr($body, 1, -1);
-    return $body;
+function gg_chuck_get_quote() {
+    $get_quote = file_get_contents('https://api.chucknorris.io/jokes/random');
+    $get_result  = json_decode($get_quote);
+    $quote = $get_result->value;
+    echo $quote;
 }
